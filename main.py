d3fe7ef7bf58f760f580721daee451e1f14f7aa4
Zaver <zavr1987@mail.ru>
2023-04-04 17:11:32 +0300

    project updated, requirements txt added

--- main.py
+++ main.py
@@ -1,33 +1,123 @@
 from fastapi import FastAPI, Request, UploadFile, File, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 from time import strftime
 from pydantic import BaseModel
+
 from model import FakeNLPModel
 import os
 
-app = FastAPI()
+handler = RotatingFileHandler(filename='app.log', maxBytes=10000, backupCount=0)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
-script_dir = os.path.dirname(__file__)
-st_abs_file_path = os.path.join(script_dir, "front/")
+app = FastAPI()
 app.mount("/front", StaticFiles(directory="front"), name="static")
 templates = Jinja2Templates(directory="front")
-
 nlp_model = FakeNLPModel()
+nlp_model.logger = logger
+
+
+@app.on_event("startup")
+def build_models():
+    try:
+        logger.info(f"{datetime()}: ----------------NEW START----------------")
+        nlp_model.init_base()
+        logger.info(f"{datetime()}: Модель поиска похожих фраз инициализирована")
+    except Exception as init_er:
+        logger.error(f"{datetime()}: Ошибка при инициализации модели поиска похожих фраз -> {init_er}")
 
 
 def datetime():
     return strftime('[%Y-%b-%d %H:%M:%S]')
 
 
+class GetItem(BaseModel):
+    result_filename: str
+    base_size: int
+    last_date: str
+    history_months: str
+    model_class: str
+    status: str
+    current_process: str
+    current_phrase: str
+    current_answer: str
+
+
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "test": 1})
+async def main_get(request: Request):
+    params = GetItem(**nlp_model.get_params())
+    return templates.TemplateResponse("index.html", {"request": request, "params": params.dict()})
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+@app.post("/history_files")
+async def read_history_files(request: Request, h_file: UploadFile | None = File(...), model_type: str = Form()):
+    if h_file.filename != "":
+        try:
+            nlp_model.get_history(h_file)
+        except Exception as update_er:
+            logger.error(f"{datetime()}: Ошибка обновления файла истории сообщений - {update_er}")
+        if (nlp_model.status == "FAIL" or nlp_model.wv_model_type != model_type) \
+                and nlp_model.history_data is not None:
+            try:
+                nlp_model.wv_model_type = model_type
+                nlp_model.build_model(model_type)
+            except Exception as model_er:
+                logger.error(f"{datetime()}: Ошибка при создании WV или KNN моделей -> {model_er}")
+    params = GetItem(**nlp_model.get_params())
+    return templates.TemplateResponse("index.html", {"request": request, "params": params.dict()})
+
+
+@app.post("/excel_predict")
+async def get_excel_predict(request: Request, target_file: UploadFile | None = File(...)):
+    if target_file.filename != "" and nlp_model.status == "OK":
+        try:
+            nlp_model.get_sim_phrases_df(file=target_file)
+        except Exception as update_er:
+            logger.error(f"{datetime()}: Ошибка при подготовке итогового датасета похохжих фраз -> {update_er}")
+    params = GetItem(**nlp_model.get_params())
+    return templates.TemplateResponse("index.html", {"request": request, "params": params.dict()
+                                                     })
+
+
+@app.get("/excel_file")
+async def get_excel_file(request: Request):
+    if nlp_model.result_filename != "":
+        result_df_path = os.path.join('.', 'data', nlp_model.result_filename)
+        if os.path.exists(result_df_path):
+            return FileResponse(result_df_path, filename=nlp_model.result_filename,
+                                media_type="application/vnd.ms-excel")
+    else:
+        nlp_model.current_process = "Остутсвует файл для скачивания"
+        params = GetItem(**nlp_model.get_params())
+        return templates.TemplateResponse("index.html", {"request": request, "params": params.dict()})
+
+
+@app.post("/sim_phrases")
+async def get_phrase_predict(request: Request,
+                             phrase: str = Form(), return_dist: bool = Form(False), num_phrases: int = Form()):
+    if num_phrases:
+        nlp_model.phrase_number = num_phrases
+    if phrase and nlp_model.status == "OK":
+        try:
+            nlp_model.get_sim_phrases_df(text=phrase, return_distance=return_dist)
+        except Exception as update_er:
+            logger.error(f"{datetime()}: Сбой поиска похожих фраз -> {update_er}")
+    params = GetItem(**nlp_model.get_params())
+    return templates.TemplateResponse("index.html", {"request": request, "params": params.dict()})
+
+
+@app.get("/logs")
+async def get_logs():
+    return FileResponse(os.path.join(".", "app.log"), media_type="text")
+
+#
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run(app, host="127.0.0.1", port=8080)
--- model.py
+++ model.py
@@ -1,12 +1,16 @@
 import re
 import random
 import pandas as pd
-from time import strftime
+from time import strftime, sleep
+from asyncio import sleep as timesleep
+import lorem
+import os
 
 
 class FakeNLPModel:
 
     def __init__(self):
+        self.current_answer = ""
         self.history_data = None
         self.data_target = None
         self.last_date = None
@@ -18,6 +22,7 @@ class FakeNLPModel:
         self.phrase_number = 20
         self.result_filename = ''
         self.wv_model_type = 'fasttext'
+        self.current_phrase = 'test query'
 
     def logger_info(self, info_txt):
         self.current_process = info_txt
@@ -28,20 +33,29 @@ class FakeNLPModel:
     def datetime():
         return strftime('[%Y-%b-%d %H:%M:%S]')
 
+    def init_base(self):
+        self.build_model(self.wv_model_type)
+        self.history_data = True
+
     def get_params(self):
+
         params_dict = {
+
+            "current_answer": "\n\n".join(self.current_answer),
             'result_filename': str(self.result_filename),
-            'base_size': self.result_filename,
+            'base_size': int(self.history_base_size),
             'last_date': str(self.last_date),
-            'history_months': '-' + '<br>-'.join(self.history_list),
+            'history_months': '-' + '<br>-'.join(self.history_list) if self.history_list else '-',
             'model_class': self.wv_model_type,
             'status': str(self.status),
             'current_process': str(self.current_process),
+            'current_phrase': str(self.current_phrase),
         }
         return params_dict
 
-    def get_history(self, filename: str):
-        if re.match(r'.+xlsx$', filename):
+    def get_history(self, filename):
+        if re.match(r'.+xlsx$', filename.filename):
+            timesleep(5)
             self.history_data = True
             self.last_date = f'{random.randint(1, 31)}-{random.randint(1, 13)}-{random.randint(2022, 2023)}'
             self.history_list = ['20-2022']
@@ -49,18 +63,24 @@ class FakeNLPModel:
 
     def build_model(self, model_type):
         if self.history_data:
+            sleep(5)
             self.wv_model_type = model_type
+            self.status = "OK"
 
-    def get_sim_phrases(self, text=None, file=None, return_distance=False):
+    def get_sim_phrases_df(self, text=None, file=None, return_distance=False):
         if text:
             if return_distance:
-                ['stroka ' + str(i) + ' :' + str(round(random.random(), 2)) for i in range(1, self.phrase_number + 1)]
+                self.current_answer = [lorem.sentence() + ' :' +
+                                       str(round(random.random(), 2)) for _ in range(1, self.phrase_number + 1)]
             else:
-                ['stroka ' + str(i) for i in range(1, self.phrase_number + 1)]
+                self.current_answer = [lorem.sentence() for _ in range(1, self.phrase_number + 1)]
+
         elif file:
             result_file = pd.DataFrame({'message1': ['text1'], 'message2': ['text2']})
             self.result_filename = str(random.randint(1, 1000)) + '.xlsx'
-            result_file.to_excel(self.result_filename)
+            result_file.to_excel(os.path.join(".", "data", self.result_filename))
+            timesleep(3)
 
         else:
             raise Exception('Введены неверные данные или загружен НЕ Excel файл')
+
